6ab8f3b28307f154225a4e963241a4145322bf4b
yurialeksndr <yuriatp@gmail.com>
2018-08-17 16:49:27 -0300

    Adição de marcadores para cada laboratório da rede (hard-coded). Adição de um quadro informativo por cima do mapa, ainda em testes.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-var mainmap = L.map('mainmap').setView([-23.550190, -46.633357], 12);
+labs = [
+  [-23.5801259, -46.3898429],
+  [-23.6221722, -46.5907948],
+  [-23.5982488, -46.7443205],
+  [-23.561983, -46.6410815],
+  [-23.5434534, -46.6390541],
+  [-23.5357699, -46.45534070000001],
+  [-23.5258714, -46.5484253],
+  [-23.5714751, -46.6402144],
+  [-23.6869926, -46.7495015],
+  [-23.4397843, -46.7871379],
+  [-23.4760977, -46.6702593],
+  [-23.478372, -46.3813019]
+]
+
+var mainmap = L.map('mainmap').setView([-23.550190, -46.633357], 11);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,3 +21,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.outdoors',
     accessToken: 'pk.eyJ1IjoiZmFibGFibGl2cmVzcCIsImEiOiJjamt5NGE2NnQwZmtpM3FsN3FqcW0wamRxIn0.o8WeEKKTVHgdYL_MaqtZ7g'
 }).addTo(mainmap);
+
+mainmap.zoomControl.setPosition('topright');
+
+for (lab in labs) {
+    L.marker(labs[lab]).addTo(mainmap);
+}
+
+$( document ).ready(function() {
+	$("#info").delay(500).animate({"opacity" : 1}, 800);
+});
